ae64f180dac17c221aea1e8d230c625b7a205875
saranonearth <saran.suresh@outlook.com>
2020-06-30 17:25:53 +0530

    Media Mention Content added

--- client/src/Components/MediaMentions.tsx
+++ client/src/Components/MediaMentions.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+
+import I1 from "../Assets/Images/MediaMention/I1.jpg";
+import I2 from "../Assets/Images/MediaMention/I2.jpg";
+import I3 from "../Assets/Images/MediaMention/I3.jpg";
+import I4 from "../Assets/Images/MediaMention/I4.jpg";
+
 interface Props {}
 
 const MediaMentions = (props: Props) => {
   return (
     <Carousel>
       <div>
-        <img src="https://via.placeholder.com/1920x1080" alt="media" />
+        <img src={I1} alt="media" />
       </div>
       <div>
-        <img src="https://via.placeholder.com/1920x1080" alt="media" />
+        <img src={I2} alt="media" />
       </div>
       <div>
-        <img src="https://via.placeholder.com/1920x1080" alt="media" />
+        <img src={I3} alt="media" />
       </div>
       <div>
-        <img src="https://via.placeholder.com/1920x1080" alt="media" />
+        <img src={I4} alt="media" />
       </div>
     </Carousel>
   );
